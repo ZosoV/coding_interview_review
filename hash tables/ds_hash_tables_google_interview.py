03a131965e075ec2bf4df62cea0841ef840b6c81
Oscar Guarnizo <oscar.guarnizo9619@gmail.com>
2022-01-31 21:36:22 -0500

    Hash Tables

--- hash tables/ds_hash_tables_google_interview.py
+++ hash tables/ds_hash_tables_google_interview.py
@@ -8,29 +8,29 @@
 #Given an array = [2,3,4,5]
 # It should return undefined
 
-def firsOcurrennce1(arr): # -> O(1) in space but O(n^2) in time
-    for i in range(1, len(arr)):
-        for j in range(0,i):
-            if (arr[i] == arr[j]):
-                return arr[i]
-
-
-print(firsOcurrennce1([2,5,1,2,3,5,1,2,4]))
-print(firsOcurrennce1([2,1,1,2,3,5,1,2,4]))
-print(firsOcurrennce1([2,3,4,5]))
-
 def firsOcurrennce(arr):
 
-    ocurrences = set() # -> O(n) in space
+    ocurrences = dict() # -> O(n) in space
 
     for num in arr: # -> O(n) in time
         if num in ocurrences:
             return num
         else:
-            ocurrences.add(num)
+            ocurrences[num] = True
     
     return None
 
 print(firsOcurrennce([2,5,1,2,3,5,1,2,4]))
 print(firsOcurrennce([2,1,1,2,3,5,1,2,4]))
 print(firsOcurrennce([2,3,4,5]))
+
+def naive_approach(arr): # -> O(1) in space but O(n^2) in time
+    for i in range(1, len(arr)):
+        for j in range(0,i):
+            if (arr[i] == arr[j]):
+                return arr[i]
+
+
+print(naive_approach([2,5,1,2,3,5,1,2,4]))
+print(naive_approach([2,1,1,2,3,5,1,2,4]))
+print(naive_approach([2,3,4,5]))
